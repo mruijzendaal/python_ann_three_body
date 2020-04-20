405a9dd6b117d65254c07c4f2c11eb4df4529bf2
Martijn Ruijzendaal <martijn.ruijzendaal@gmail.com>
2020-04-20 11:38:59 +0200

    Facilitate storage of datasets

--- ann_three_body/storage/__init__.py
+++ ann_three_body/storage/__init__.py
@@ -2,16 +2,26 @@ import pickle
 import os
 
 
-def store_simulation(m, r_init, v_init, T_max, r_out, v_out, filename, folder="output"):
+def store_simulation(m, r_init, v_init, t, r_out, v_out, filename, folder="output"):
     if not os.path.isdir(folder):
         os.mkdir(folder)
     filename += ".pkl"
     file = os.path.join(folder, filename)
-    pickle.dump([m, r_init, v_init, T_max, r_out, v_out], open(file, "wb"))
+    pickle.dump([m, r_init, v_init, t, r_out, v_out], open(file, "wb"))
 
 
 def load_simulation(filename, folder="output"):
-    filename += ".pkl"
+    if not filename.endswith(".pkl"):
+        filename += ".pkl"
     file = os.path.join(folder, filename)
     m, r_init, v_init, T_max, r_out, v_out = pickle.load(open(file, "rb"))
     return m, r_init, v_init, T_max, r_out, v_out
+
+
+def load_sets_from_folder(folder):
+    sets = []
+    for f in os.listdir(folder):
+        if not os.path.isfile(os.path.join(folder, f)) or not f.endswith(".pkl"):
+            continue
+        sets.append(load_simulation(f, folder=folder))
+    return sets
